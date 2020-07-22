7bbf57f6e3b565904d0bfe94c81e46c893aa7007
Hu <HUXR1@legal.regn.net>
2020-07-22 15:34:33 +0800

    highlight

--- src/components/DragHandle/index.vue
+++ src/components/DragHandle/index.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="editor" @dragstart="enableEditing()" @dragend="disableEditing()">
     <editor-content class="editor__content" :editor="editor" />
+    <editor-menu-bubble class="menububble" :editor="editor" v-slot="{ commands, isActive, menu }">
+      <div
+        :class="{ 'is-active': menu.isActive }"
+        :style="`left: ${menu.left}px; bottom: ${menu.bottom}px;`"
+      >
+        <button class="menububble__button" @click="addHeadnote()">New headnote</button>
+        <button class="menububble__button" @click="commands.highlight">Highlight</button>
+      </div>
+    </editor-menu-bubble>
   </div>
 </template>
 
 <script>
-import { Editor, EditorContent } from "tiptap";
-import { Heading, Code } from "tiptap-extensions";
+import { Editor, EditorContent, EditorMenuBubble } from "tiptap";
+import { Highlight } from "../Highlight/Highlight";
 import DragItem from "./DragItem";
 
 export default {
   components: {
-    EditorContent
+    EditorContent,
+    EditorMenuBubble
   },
   data() {
     return {
+      keepInBounds: true,
       editor: new Editor({
-        extensions: [new Heading(), new Code(), new DragItem()],
-        editable: false,
+        extensions: [new Highlight(), new DragItem()],
+        editable: true,
         content: `
           <h2>
             HN Number Drag Drop
@@ -39,15 +50,20 @@ export default {
   methods: {
     enableEditing() {
       this.editor.setOptions({
-        editable: true,
-      })
-      console.log(this.editor);
+        editable: true
+      });
     },
     disableEditing() {
       this.editor.setOptions({
-        editable: false,
-      })
-      console.log(this.editor);
+        editable: false
+      });
+    },
+    addHeadnote() {
+      var newHnNum = this.editor.schema.nodes.hn_num.create(null, null, null);
+      var position = this.editor.selection.from;
+      var transaction = this.editor.state.tr.insert(position, newHnNum);
+      this.editor.dispatchTransaction(transaction);      
+      this.editor.commands.highlight()
     }
   }
 };
@@ -67,5 +83,60 @@ export default {
 .editor {
   text-align: left;
   font-size: 16px;
+  position: relative;
+}
+
+.menububble {
+  position: absolute;
+  display: flex;
+  z-index: 20;
+  background: black;
+  border-radius: 5px;
+  padding: 0.3rem;
+  margin-bottom: 0.5rem;
+  transform: translateX(-50%);
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 0.2s, visibility 0.2s;
+
+  &.is-active {
+    opacity: 1;
+    visibility: visible;
+  }
+
+  &__button {
+    display: inline-flex;
+    background: transparent;
+    border: 0;
+    color: white;
+    padding: 0.2rem 0.5rem;
+    margin-right: 0.2rem;
+    border-radius: 3px;
+    cursor: pointer;
+
+    &:last-child {
+      margin-right: 0;
+    }
+
+    &:hover {
+      background-color: rgba(white, 0.3);
+    }
+
+    &.is-active {
+      background-color: rgba(white, 0.3);
+    }
+  }
+
+  &__form {
+    display: flex;
+    align-items: center;
+  }
+
+  &__input {
+    font: inherit;
+    border: none;
+    background: transparent;
+    color: white;
+  }
 }
 </style>
